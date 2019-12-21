77588a882fd94f68a713112bf5f5dd71668697ac
mitesh293 <pmitesh293@gmail.com>
2019-12-22 01:32:25 +0530

    first commit

---
+++ src/main/java/com/example/GradleDemo/controller/CabApiController.java
@@ -0,0 +1,11 @@
+package com.example.GradleDemo.controller;
+
+import com.mit.CabApiDto;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CabApiController {
+    public void getCanApiController(){
+        CabApiDto cabApiDto=new CabApiDto();
+    }
+}
